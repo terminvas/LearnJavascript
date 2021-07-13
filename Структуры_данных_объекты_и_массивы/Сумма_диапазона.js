07e5cec1d67300aedaade4bb9b784068ac872499
terminvas <87269496+terminvas@users.noreply.github.com>
2021-07-13 16:04:22 +0300

    Delete Сумма_диапазона.js

--- Структуры_данных_объекты_и_массивы/Сумма_диапазона.js
+++
@@ -1,8 +0,0 @@
-function range(firstIndex, endIndex, step) {
-   let result = [];
-   for (let index = firstIndex; (index <= Math.max(firstIndex, endIndex)) && (index >= Math.min(firstIndex, endIndex)); index += step) {
-      result.push(index);
-   };
-   return result
-};
-console.log(range(-8, 17, 3))
