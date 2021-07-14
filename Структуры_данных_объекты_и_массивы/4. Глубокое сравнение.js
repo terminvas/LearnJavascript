1a5a37ba8015e7337398db933695d87da5a87b01
terminvas <87269496+terminvas@users.noreply.github.com>
2021-07-14 20:18:29 +0300

    master

--- Структуры_данных_объекты_и_массивы/4. Глубокое сравнение.js
+++ Структуры_данных_объекты_и_массивы/4. Глубокое сравнение.js
@@ -16,7 +16,7 @@ function deepEqual(a, b) {
    return true;
 }
 
-let obj = { here: { is: "an" }, object: 2 };
+let obj = { here: { is: "an" }, object: 3 };
 console.log(deepEqual(obj, obj));
 // → true
 console.log(deepEqual(obj, { here: 1, object: 2 }));
